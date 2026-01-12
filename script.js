6575ffddc587b3f6ab261e289743adeb359b329b
skyesu <sbs.garcia2004@gmail.com>
2026-01-11 20:03:28 -0500

    more_readings_and_css

---
+++ script.js
@@ -0,0 +1,33 @@
+let modals = document.getElementsByClassName('modal');
+let modalBtns = document.getElementsByClassName('modal-btn');
+let closeBtns = document.getElementsByClassName('close');
+
+for(let modalBtn of modalBtns) {
+    modalBtn.onclick = function(event) {
+        document.querySelector(event.target.getAttribute('href') ).style.display = 'block';
+    };
+}
+
+for(let closeBtn of closeBtns) {
+    closeBtn.onclick = function(event) {
+        event.target.parentNode.parentNode.style.display = 'none';
+    };
+}
+
+window.onclick = function(event) {
+    if(event.target.classList.contains('modal') ) {
+        for(let modal of modals) {
+            if(typeof modal.style !== 'undefined') {
+                modal.style.display = 'none';    
+            }
+        }
+    }
+};
+
+window.onkeydown = function(event) {
+    if (event.key == 'Escape') {
+        for(let modal of modals) {
+            modal.style.display = 'none';
+        }
+    }
+};
